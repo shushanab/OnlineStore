493a9daa21a7f4e0939dcfe5bb690e8a4445318e
Shushan Abrahamyan <abrahamyan.shushan.career@gmail.com>
2024-05-23 00:44:57 +0400

    refactor: update directory structure and formatted code

---
+++ components/CategoriesHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click.stop="toggleExpansion" class="expansion-panel-header">
+    <v-row>
+      <v-card-title>
+        {{ title }}
+      </v-card-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="toggleExpansion">
+        <v-icon>{{ expanded ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    expanded: Boolean,
+    title: String,
+  });
+
+  const emit = defineEmits(['update:expanded']);
+
+  const toggleExpansion = () => {
+    console.log('update:expanded', !props.expanded);
+
+    emit('update:expanded', !props.expanded);
+  };
+</script>
+
+<style scoped>
+  .expansion-panel-header {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+</style>
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: 'auto',
+  vueIndentScriptAndStyle: true,
+};
